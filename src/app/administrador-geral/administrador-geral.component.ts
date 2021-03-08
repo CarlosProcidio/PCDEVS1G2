ed291312663cd56c564a0806ba56dc4c3ea37175
CarlosProcidio <carlosprocidio@gmail.com>
2021-03-08 10:11:42 -0300

    Finalização da Config. do Projeto

---
+++ src/app/administrador-geral/administrador-geral.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-administrador-geral',
+  templateUrl: './administrador-geral.component.html',
+  styleUrls: ['./administrador-geral.component.css']
+})
+export class AdministradorGeralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdministradorGeralComponent } from './administrador-geral/administrador-geral.component';
+import { PrefeituraComponent } from './prefeitura/prefeitura.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { HotelComponent } from './hotel/hotel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdministradorGeralComponent,
+    PrefeituraComponent,
+    ClienteComponent,
+    HotelComponent
   ],
   imports: [
     BrowserModule

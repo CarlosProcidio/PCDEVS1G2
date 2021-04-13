9fe8b1ce3e4390ed19feafa2ca6bfbb35fdef927
Fernando Borba <fsborba5@gmail.com>
2021-04-12 22:34:56 -0300

    export galleriaAutoplay

--- src/app/hotel/hotel.component.ts
+++ src/app/hotel/hotel.component.ts
@@ -13,3 +13,28 @@ export class HotelComponent implements OnInit {
   }
 
 }
+// implantado junto com o html do primeng 
+export class GalleriaAutoplayDemo implements OnInit {
+  images: any[];
+
+  responsiveOptions:any[] = [
+      {
+          breakpoint: '1024px',
+          numVisible: 5
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 3
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1
+      }
+  ];
+
+  constructor(private photoService: PhotoService) { }
+
+  ngOnInit() {
+      this.photoService.getImages().then(images => this.images = images)
+  }
+}

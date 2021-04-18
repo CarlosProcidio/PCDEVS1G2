10b6d2011cb582b2595a8d6d5903ffd5966cea91
CarlosProcidio <carlosprocidio@gmail.com>
2021-04-18 19:18:40 -0300

    Modulo Estabelecimento

---
+++ src/app/estabelecimentos/estabelecimentos.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EstabelecimentosComponent } from './estabelecimentos.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [EstabelecimentosComponent]
+})
+export class EstabelecimentosModule { }

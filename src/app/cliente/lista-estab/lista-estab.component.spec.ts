f98472fa1951ab09df5f1565e948db8266b2b078
Flagner Vaz Camargo <flagner.camargo@serpro.gov.br>
2021-04-19 18:58:40 -0300

    lista-estab e consulta-estab, ajuste db e header.

---
+++ src/app/cliente/lista-estab/lista-estab.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaEstabComponent } from './lista-estab.component';
+
+describe('ListaEstabComponent', () => {
+  let component: ListaEstabComponent;
+  let fixture: ComponentFixture<ListaEstabComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ListaEstabComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ListaEstabComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

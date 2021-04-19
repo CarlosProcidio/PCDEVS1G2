bed72f12eb6ad08652bc3f08961e68f05178fb18
Flagner Vaz Camargo <flagner.camargo@serpro.gov.br>
2021-04-19 00:53:06 -0300

    rotas p/ consulta e lista.

--- src/app/administrador/administrador.routing.ts
+++ src/app/administrador/administrador.routing.ts
@@ -1,3 +1,5 @@
+import { ListaComponent } from './../estabelecimentos/lista/lista.component';
+import { ConsultaComponent } from './../estabelecimentos/consulta/consulta.component';
 import { HomeComponent } from './../home/home.component';
 import { HotelComponent } from './../hotel/hotel.component';
 import { FormularioComponent } from './formulario/formulario.component';
@@ -11,7 +13,9 @@ const routes: Routes = [
   { path: 'listar/administrador' , component:TabelaComponent },
   { path: 'administradores/:id' , component:TabelaComponent },
   { path: 'home' , component:HomeComponent},
-  { path: 'hotel' , component:HotelComponent}
+  { path: 'hotel' , component:HotelComponent},
+  { path: 'consulta' , component:ConsultaComponent},
+  { path: 'lista' , component:ListaComponent}
 ];
 
 export const AdministradorRoutes = RouterModule.forChild(routes);
